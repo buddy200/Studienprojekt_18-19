f3198e5b54720562f53c0d9a20d4abe32c6845ae
Lars Buttgereit <st141897@stud.uni-stuttgart.de>
2017-11-22 11:34:58 +0100

    neue Oberklasse für Field und DamageCase erstellt

---
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/data/Damage_Case.java
@@ -0,0 +1,10 @@
+package de.uni_stuttgart.informatik.sopra.sopraapp.data.geoData;
+
+import de.uni_stuttgart.informatik.sopra.sopraapp.data.Field_DamageCase;
+
+/**
+ * Created by larsb on 22.11.2017.
+ */
+
+public class Damage_Case extends Field_DamageCase {
+}
---
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/data/Field_DamageCase.java
@@ -0,0 +1,8 @@
+package de.uni_stuttgart.informatik.sopra.sopraapp.data;
+
+/**
+ * Created by larsb on 22.11.2017.
+ */
+
+class Field_DamageCase {
+}

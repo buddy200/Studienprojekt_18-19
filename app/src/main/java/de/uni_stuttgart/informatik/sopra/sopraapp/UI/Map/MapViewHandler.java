3e95fff1d1a450faff4c89cbe47eb4bd7181dbdb
Felix B <felix.burk@gmai.com>
2018-01-08 13:19:55 +0100

    icons added

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/UI/Map/MapViewHandler.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/UI/Map/MapViewHandler.java
@@ -85,6 +85,7 @@ public class MapViewHandler implements MapContract.MapHandler {
         mapController = map.getController();
         mapController.setZoom(GlobalConstants.DEFAULT_ZOOM);
         mapController.setCenter(GlobalConstants.START_POINT);
+       // mapController.setInvertedTiles(true);
 
         //setup listener for tabs on polygons
         if (context instanceof FragmentInteractionListener) {
@@ -246,6 +247,7 @@ public class MapViewHandler implements MapContract.MapHandler {
 
     public void destroy(){
         map.onDetach();
+
     }
 
 }

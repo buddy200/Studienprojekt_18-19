f82bef15c1ea6053f1b83254026774b1ee6277f0
Lars Buttgereit <st141897@stud.uni-stuttgart.de>
2018-01-24 09:47:52 +0100

    intersection calculator reworked 1. fixed photo intnent

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/fieldManager/Util/PhotoManager.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/fieldManager/Util/PhotoManager.java
@@ -48,7 +48,7 @@ public class PhotoManager {
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(activity, " de.uni_stuttgart.informatik.sopra.fieldManager.Util.fileprovider"
+                Uri photoURI = FileProvider.getUriForFile(activity, "de.uni_stuttgart.informatik.sopra.fieldManager.Util.fileprovider"
                         ,
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
